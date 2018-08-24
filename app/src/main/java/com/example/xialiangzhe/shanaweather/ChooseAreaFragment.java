4dc3de250dd8c66cf0153b1f164efc8aab075817
yzxiaozhe <652348816@qq.com>
2018-08-24 15:39:34 +0800

    加入加入显示天气信息功能。

--- app/src/main/java/com/example/xialiangzhe/shanaweather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/xialiangzhe/shanaweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.example.xialiangzhe.shanaweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Binder;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -76,6 +77,12 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel==LEVEL_CITY){
                     selectCity=cityList.get(i);
                     queryCounties();
+                }else if (currentLevel==LEVEL_COUNTY){
+                    String weatherId=countyList.get(i).getWeatherId();
+                    Intent intent=new Intent(getActivity(),WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
@@ -220,4 +227,5 @@ public class ChooseAreaFragment extends Fragment {
             progressDialog.dismiss();
         }
     }
+
 }
--- app/src/main/java/com/example/xialiangzhe/shanaweather/util/Utility.java
+++ app/src/main/java/com/example/xialiangzhe/shanaweather/util/Utility.java
@@ -6,6 +6,8 @@ import com.example.xialiangzhe.shanaweather.R;
 import com.example.xialiangzhe.shanaweather.db.City;
 import com.example.xialiangzhe.shanaweather.db.County;
 import com.example.xialiangzhe.shanaweather.db.Province;
+import com.example.xialiangzhe.shanaweather.gson.Weather;
+import com.google.gson.Gson;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -77,6 +79,21 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /*
+    将返回的JSON数据类型解析成Weather实体类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try {
+            JSONObject jsonObject=new JSONObject(response);
+            JSONArray jsonArray=jsonObject.getJSONArray("HeWeather");
+            String weatherContent=jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
